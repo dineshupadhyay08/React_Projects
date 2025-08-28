bf22412438a1cfa7473d29fad4a3087e8ae0a9c7
dineshupadhyay08 <dineshupadhyay666.com>
2025-08-28 23:24:12 +0530

    first commit

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import Navigarion from './components/Navigarion'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <div>
+      <Navigarion/>
+      <Hero/>
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ Project-1/src/components/Navigarion.jsx
@@ -0,0 +1,21 @@
+const Navigarion = () =>{
+
+    return(
+        <div className="container">
+            <nav>
+                <div className='logo'>
+                     <img src="/images/brand_logo.png" alt="logo" />
+                </div>
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+                </ul>
+                    <button>Login</button>
+            </nav>
+    </div>
+    )
+};
+
+export default Navigarion;
